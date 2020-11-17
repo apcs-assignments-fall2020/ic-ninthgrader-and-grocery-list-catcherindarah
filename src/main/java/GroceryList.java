92fcb8fc32cc86fa9ed8007a7565934a872dd31f
catcherindarah <22rray@athenian.org>
2020-11-17 01:27:30 +0000

    completed assignment

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,34 @@
+import java.util.stream.*;
+import java.util.Arrays;
+
 public class GroceryList {
-    
+    private String[] arr;
+    public GroceryList() {
+        this.arr = new String[10];
+    }
+
+    public void add(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+                break;
+            }
+        }
+    }
+    public void remove(String item) {
+        for (int i = 0; i <arr.length; i++){
+            if (arr[i].equals(item)) {
+                for ( int k = i; i < arr.length-1; k++) {
+                    arr[k] = arr[k+1];
+                }
+                arr[arr.length-1] = null;
+                break;
+            }
+        }
+    }
+    public String toString(){
+        String a = Arrays.toString(arr);
+        System.out.println("Grocery List: ");
+        return a;
+    }
 }
